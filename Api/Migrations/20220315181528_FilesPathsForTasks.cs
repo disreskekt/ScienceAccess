78fa075a76d034ab6e176218175c7b786a513dd3
Максим <disreskekt@gmail.com>
2022-03-16 14:23:28 +0300

    Migration fix

--- Api/Migrations/20220315181528_FilesPathsForTasks.cs
+++ Api/Migrations/20220315181528_FilesPathsForTasks.cs
@@ -7,16 +7,6 @@ namespace Api.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                table: "Users",
-                keyColumn: "Id",
-                keyValue: 1);
-
-            migrationBuilder.DeleteData(
-                table: "Users",
-                keyColumn: "Id",
-                keyValue: 2);
-
             migrationBuilder.AddColumn<string[]>(
                 name: "FilesPaths",
                 table: "Tasks",
@@ -29,15 +19,6 @@ namespace Api.Migrations
             migrationBuilder.DropColumn(
                 name: "FilesPaths",
                 table: "Tasks");
-
-            migrationBuilder.InsertData(
-                table: "Users",
-                columns: new[] { "Id", "Email", "Lastname", "Name", "Password", "RoleId", "TicketRequest" },
-                values: new object[,]
-                {
-                    { 1, "base@base.base", "Admin", "Base", "qwerty", 1, false },
-                    { 2, "init@init.init", "User", "Init", "qwerty", 2, false }
-                });
         }
     }
 }
