b35800c6b28dc154ff5c0043d3a7dc044583b0d7
Максим <disreskekt@gmail.com>
2022-03-20 22:10:19 +0300

    Swagger extension

--- Api/Extensions/StartupExtensions.cs
+++ Api/Extensions/StartupExtensions.cs
@@ -1,9 +1,11 @@
-﻿using Api.Options;
+﻿using System.Collections.Generic;
+using Api.Options;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace Api.Extensions
 {
@@ -30,6 +32,49 @@ namespace Api.Extensions
                         IssuerSigningKey = authOptions.GetSymmetricSecurityKey(),
                     };
                 });
+        }
+        
+        public static IServiceCollection AddSwagger(this IServiceCollection services, string projectTitle, bool useNewtonsoftJson = true)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = projectTitle, Version = "v1" });
+                c.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description =
+                            @"JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below. Example: 'Bearer 12345abcdef'",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = JwtBearerDefaults.AuthenticationScheme
+                    });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = JwtBearerDefaults.AuthenticationScheme,
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            // if (useNewtonsoftJson)
+            // {
+            //     services.AddSwaggerGenNewtonsoftSupport(); // explicit opt-in - needs to be placed after AddSwaggerGen()
+            // }
+            
+            return services;
         }
 
         public static TResult AddOptions<TResult>(this IServiceCollection services, IConfiguration configuration, string section)
